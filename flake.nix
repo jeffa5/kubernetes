400e58ee2525af45ad184c10d4c3200e4d2518d5
Andrew Jeffery <dev@jeffas.io>
2021-04-29 22:23:47 +0100

    Add nix flake config

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Kubernetes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { system = system; };
+        in
+        rec
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs;[
+              protobuf
+              kubectl
+              k9s
+
+              go
+              etcd_3_4
+              openssl
+
+              rnix-lsp
+              nixpkgs-fmt
+            ];
+          };
+        }
+      );
+}
