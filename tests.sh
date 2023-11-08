4312eed91b464a6eb320a0374c19b7315dda59ae
Andrew Jeffery <dev@jeffas.io>
2023-11-08 14:28:11 +0000

    Add scheduler to tests script

--- tests.sh
+++ tests.sh
@@ -2,9 +2,10 @@
 
 function run_tests() {
   echo "Running tests for $1"
-  make test-integration WHAT=./test/integration/$1 GOFLAGS="-v -failfast" &> $1.out
+  make test-integration WHAT=./test/integration/$1 GOFLAGS="-v -failfast -run=$2" &> $1.out
 }
 
 run_tests statefulset
 run_tests deployment
 run_tests replicaset
+run_tests scheduler "TestUnschedulableNodes|TestAllocatable"
